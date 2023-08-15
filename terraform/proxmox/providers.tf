7fddce88af98e4a3750a081703da463825ba2d29
Nikolay Asenov <asenov.nikolay98@gmail.com>
2023-08-15 21:36:14 +0300

    use terraform vars

--- terraform/proxmox/providers.tf
+++ terraform/proxmox/providers.tf
@@ -1,4 +1,7 @@
 provider "proxmox" {
-  pm_api_url = "http://192.168.1.208:8006/api2/json"
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+
   pm_tls_insecure = true
 }
