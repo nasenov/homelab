0a4a772402f59625ee3ccbb89fdde97c59e9cb64
Nikolay Asenov <asenov.nikolay98@gmail.com>
2023-08-30 20:32:59 +0300

    remove unused vars

--- terraform/proxmox/variables.tf
+++ terraform/proxmox/variables.tf
@@ -2,16 +2,6 @@ variable "proxmox_api_url" {
   type = string
 }
 
-variable "proxmox_api_token_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "proxmox_api_token_secret" {
-  type      = string
-  sensitive = true
-}
-
 variable "proxmox_user" {
   type      = string
   sensitive = true
