0827844ba7921848c1d063e9c161354f37fa7886
Nikolay Asenov <asenov.nikolay98@gmail.com>
2024-08-04 13:22:46 +0300

    chore: sunset storage k3s cluster

--- hack/init-storage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-scp admin@192.168.1.100:/etc/rancher/k3s/k3s.yaml ~/.kube/config-storage
-
-chmod 0600 ~/.kube/config-storage
-
-kubectl --kubeconfig ~/.kube/config-storage config set-cluster default --server=https://192.168.1.100:6443
-
