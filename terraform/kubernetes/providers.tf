5fffc1360640e50bf4fe73777f72731f670eccb7
Nikolay Asenov <asenov.nikolay98@gmail.com>
2024-10-06 19:17:52 +0300

    feat: init terraform providers

---
+++ terraform/kubernetes/providers.tf
@@ -0,0 +1,18 @@
+provider "proxmox" {
+  endpoint = var.proxmox_api_url
+  username = var.proxmox_username
+  password = var.proxmox_password
+  insecure = true
+
+  ssh {
+    agent = true
+    node {
+      name    = "pve"
+      address = "192.168.1.10"
+    }
+  }
+}
+
+provider "talos" {}
+
+provider "local" {}
---
+++ terraform/kubernetes/variables.tf
@@ -0,0 +1,13 @@
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_password" {
+  type      = string
+  sensitive = true
+}
