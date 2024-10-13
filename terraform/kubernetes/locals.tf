66b1c96d3010d181e568cecb375d49fb69065f75
Nikolay Asenov <asenov.nikolay98@gmail.com>
2024-10-13 19:14:37 +0300

    chore: follow terraform file naming convention

---
+++ terraform/kubernetes/locals.tf
@@ -0,0 +1,127 @@
+locals {
+  virtual_machines = {
+    k8s-1 = {
+      ipv4_address = "192.168.1.21/24"
+      cpu_cores    = 2
+      memory       = 2048
+    }
+    k8s-2 = {
+      ipv4_address = "192.168.1.22/24"
+      cpu_cores    = 2
+      memory       = 2048
+    }
+    k8s-3 = {
+      ipv4_address = "192.168.1.23/24"
+      cpu_cores    = 2
+      memory       = 2048
+    }
+    k8s-4 = {
+      ipv4_address = "192.168.1.24/24"
+      cpu_cores    = 1
+      memory       = 1024
+    }
+    k8s-5 = {
+      ipv4_address = "192.168.1.25/24"
+      cpu_cores    = 1
+      memory       = 1024
+    }
+    k8s-6 = {
+      ipv4_address = "192.168.1.26/24"
+      cpu_cores    = 1
+      memory       = 1024
+    }
+    k8s-7 = {
+      ipv4_address = "192.168.1.27/24"
+      cpu_cores    = 1
+      memory       = 1024
+    }
+    k8s-8 = {
+      ipv4_address = "192.168.1.28/24"
+      cpu_cores    = 1
+      memory       = 1024
+    }
+    k8s-9 = {
+      ipv4_address = "192.168.1.29/24"
+      cpu_cores    = 1
+      memory       = 1024
+    }
+    k8s-10 = {
+      ipv4_address = "192.168.1.30/24"
+      cpu_cores    = 1
+      memory       = 1024
+    }
+  }
+}
+
+locals {
+  talos_install_image_config_patch = yamlencode({
+    machine = {
+      install = {
+        image = "factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.8.0"
+      }
+    }
+  })
+
+  talos_vip_config_patch = yamlencode({
+    machine = {
+      network = {
+        interfaces = [
+          {
+            interface = "eth0"
+            vip = {
+              ip = "192.168.1.20"
+            }
+          }
+        ]
+      }
+    }
+  })
+}
+
+locals {
+  controlplane_virtual_machines = {
+    k8s_1 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-1"].name
+      endpoint = "192.168.1.21"
+    }
+    k8s_2 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-2"].name
+      endpoint = "192.168.1.22"
+    }
+    k8s_3 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-3"].name
+      endpoint = "192.168.1.23"
+    }
+  }
+
+  worker_virtual_machines = {
+    k8s_4 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-4"].name
+      endpoint = "192.168.1.24"
+    }
+    k8s_5 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-5"].name
+      endpoint = "192.168.1.25"
+    }
+    k8s_6 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-6"].name
+      endpoint = "192.168.1.26"
+    }
+    k8s_7 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-7"].name
+      endpoint = "192.168.1.27"
+    }
+    k8s_8 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-8"].name
+      endpoint = "192.168.1.28"
+    }
+    k8s_9 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-9"].name
+      endpoint = "192.168.1.29"
+    }
+    k8s_10 = {
+      node     = proxmox_virtual_environment_vm.k8s["k8s-10"].name
+      endpoint = "192.168.1.30"
+    }
+  }
+}
--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -1,26 +1,141 @@
-terraform {
-  cloud {
-    organization = "nasenov"
+resource "proxmox_virtual_environment_vm" "k8s" {
+  for_each = local.virtual_machines
 
-    workspaces {
-      name = "kubernetes"
-    }
+  name      = each.key
+  node_name = "pve"
+
+  machine       = "q35"
+  scsi_hardware = "virtio-scsi-single"
+  started       = true
+
+  cpu {
+    type    = "x86-64-v2-AES"
+    sockets = 1
+    cores   = each.value.cpu_cores
+    units   = 100
   }
 
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.66.2"
-    }
+  memory {
+    dedicated = each.value.memory
+    floating  = each.value.memory
+  }
 
-    talos = {
-      source  = "siderolabs/talos"
-      version = "0.6.0"
-    }
+  network_device {
 
-    local = {
-      source = "hashicorp/local"
-      version = "2.5.2"
+  }
+
+  disk {
+    datastore_id = "fast"
+    file_id      = "local:iso/nocloud-amd64.img"
+    file_format  = "raw"
+    interface    = "scsi0"
+    iothread     = true
+    size         = 10
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  agent {
+    enabled = true
+  }
+
+  initialization {
+    datastore_id = "local-lvm"
+
+    ip_config {
+      ipv4 {
+        address = each.value.ipv4_address
+        gateway = "192.168.1.1"
+      }
     }
   }
+}
+
+resource "talos_machine_secrets" "this" {}
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = "homelab"
+  machine_type     = "controlplane"
+  cluster_endpoint = "https://192.168.1.20:6443"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  for_each = local.controlplane_virtual_machines
+
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+  node                        = each.value.node
+  endpoint                    = each.value.endpoint
+  config_patches = [
+    local.talos_install_image_config_patch,
+    local.talos_vip_config_patch
+  ]
+}
+
+data "talos_machine_configuration" "worker" {
+  cluster_name     = "homelab"
+  machine_type     = "worker"
+  cluster_endpoint = "https://192.168.1.20:6443"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+resource "talos_machine_configuration_apply" "worker" {
+  for_each = local.worker_virtual_machines
+
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
+  node                        = each.value.node
+  endpoint                    = each.value.endpoint
+  config_patches              = [local.talos_install_image_config_patch]
+}
+
+resource "talos_machine_bootstrap" "this" {
+  depends_on = [
+    talos_machine_configuration_apply.controlplane
+  ]
+  node                 = "192.168.1.21"
+  client_configuration = talos_machine_secrets.this.client_configuration
+}
+
+resource "talos_cluster_kubeconfig" "this" {
+  depends_on = [
+    talos_machine_bootstrap.this
+  ]
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = "192.168.1.21"
+}
+
+resource "local_sensitive_file" "kubeconfig" {
+  content  = talos_cluster_kubeconfig.this.kubeconfig_raw
+  filename = pathexpand("~/.kube/config")
+}
+
+data "talos_client_configuration" "this" {
+  cluster_name         = "homelab"
+  client_configuration = talos_machine_secrets.this.client_configuration
+  endpoints = [
+    "192.168.1.21",
+    "192.168.1.22",
+    "192.168.1.23"
+  ]
+  nodes = [
+    "192.168.1.21",
+    "192.168.1.22",
+    "192.168.1.23",
+    "192.168.1.24",
+    "192.168.1.25",
+    "192.168.1.26",
+    "192.168.1.27",
+    "192.168.1.28",
+    "192.168.1.29",
+    "192.168.1.30"
+  ]
+}
+
+resource "local_sensitive_file" "talosconfig" {
+  content  = data.talos_client_configuration.this.talos_config
+  filename = pathexpand("~/.talos/config")
 }
