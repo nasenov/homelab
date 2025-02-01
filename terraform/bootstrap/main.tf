9b2aa6c6ba5e466f05a71505482ef6c54238a62f
Nikolay Asenov <asenov.nikolay98@gmail.com>
2025-02-01 21:42:21 +0200

    feat: one command k8s cluster bootstrap

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,51 @@
+resource "helm_release" "prometheus_operator_crds" {
+  name             = "prometheus-operator-crds"
+  namespace        = "monitoring"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus-operator-crds"
+  version          = "17.0.2"
+  create_namespace = true
+}
+
+resource "helm_release" "cilium" {
+  depends_on = [helm_release.prometheus_operator_crds]
+
+  name             = "cilium"
+  namespace        = "kube-system"
+  repository       = "https://helm.cilium.io"
+  chart            = "cilium"
+  version          = "1.16.6"
+  wait_for_jobs    = true
+
+  values = [
+    "${file("../../kubernetes/apps/kube-system/cilium/app/helm-values.yaml")}"
+  ]
+}
+
+resource "helm_release" "flux_operator" {
+  depends_on = [helm_release.cilium]
+
+  name             = "flux-operator"
+  namespace        = "flux-system"
+  repository       = "oci://ghcr.io/controlplaneio-fluxcd/charts"
+  chart            = "flux-operator"
+  version          = "0.13.0"
+
+  values = [
+    "${file("../../kubernetes/apps/flux-system/flux-operator/app/values.yaml")}"
+  ]
+}
+
+resource "helm_release" "flux_instance" {
+  depends_on = [helm_release.flux_operator]
+
+  name       = "flux-instance"
+  namespace  = "flux-system"
+  repository = "oci://ghcr.io/controlplaneio-fluxcd/charts"
+  chart      = "flux-instance"
+  version    = "0.13.0"
+
+  values = [
+    "${file("../../kubernetes/apps/flux-system/flux-operator/instance/values.yaml")}"
+  ]
+}
---
+++ terraform/bootstrap/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  cloud {
+    organization = "nasenov"
+
+    workspaces {
+      name = "bootstrap"
+    }
+  }
+
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
+}
