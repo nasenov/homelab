9a6382f48a4b34c0a8afc38c19c0c9f510db238c
Nikolay Asenov <asenov.nikolay98@gmail.com>
2025-05-11 13:48:46 +0300

    chore(cloudflare): remove unused variable

--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -1,21 +1,13 @@
-variable "cloudflare_zone" {
-  description = "Domain used to expose the GCP VM instance to the Internet"
-  type        = string
+variable "cloudflare_account_id" {
+  type      = string
+  sensitive = true
 }
 
 variable "cloudflare_zone_id" {
-  description = "Zone ID for your domain"
-  type        = string
-}
-
-variable "cloudflare_account_id" {
-  description = "Account ID for your Cloudflare account"
-  type        = string
-  sensitive   = true
+  type = string
 }
 
 variable "cloudflare_token" {
-  description = "Cloudflare API token"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
