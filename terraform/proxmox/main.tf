657dc4c3a4fbd74eb4f152bded439b5b68dc91d7
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2025-08-06 21:12:16 +0300

    chore: apply proxmox terraform

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,31 +1,31 @@
-resource "proxmox_virtual_environment_apt_standard_repository" "no_subscription" {
-  node   = "pve"
-  handle = "no-subscription"
-}
+# resource "proxmox_virtual_environment_apt_standard_repository" "no_subscription" {
+#   node   = "pve"
+#   handle = "no-subscription"
+# }
 
-resource "proxmox_virtual_environment_apt_standard_repository" "enterprise" {
-  node   = "pve"
-  handle = "enterprise"
-}
+# resource "proxmox_virtual_environment_apt_standard_repository" "enterprise" {
+#   node   = "pve"
+#   handle = "enterprise"
+# }
 
-resource "proxmox_virtual_environment_apt_repository" "enterprise" {
-  enabled   = false
-  file_path = proxmox_virtual_environment_apt_standard_repository.enterprise.file_path
-  index     = proxmox_virtual_environment_apt_standard_repository.enterprise.index
-  node      = proxmox_virtual_environment_apt_standard_repository.enterprise.node
-}
+# resource "proxmox_virtual_environment_apt_repository" "enterprise" {
+#   enabled   = false
+#   file_path = proxmox_virtual_environment_apt_standard_repository.enterprise.file_path
+#   index     = proxmox_virtual_environment_apt_standard_repository.enterprise.index
+#   node      = proxmox_virtual_environment_apt_standard_repository.enterprise.node
+# }
 
-resource "proxmox_virtual_environment_apt_standard_repository" "ceph_quincy_enterprise" {
-  node   = "pve"
-  handle = "ceph-quincy-enterprise"
-}
+# resource "proxmox_virtual_environment_apt_standard_repository" "ceph_quincy_enterprise" {
+#   node   = "pve"
+#   handle = "ceph-quincy-enterprise"
+# }
 
-resource "proxmox_virtual_environment_apt_repository" "ceph_quincy_enterprise" {
-  enabled   = false
-  file_path = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.file_path
-  index     = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.index
-  node      = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.node
-}
+# resource "proxmox_virtual_environment_apt_repository" "ceph_quincy_enterprise" {
+#   enabled   = false
+#   file_path = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.file_path
+#   index     = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.index
+#   node      = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.node
+# }
 
 resource "proxmox_virtual_environment_acme_account" "letsencrypt" {
   name      = "letsencrypt"
@@ -228,8 +228,8 @@ resource "proxmox_virtual_environment_vm" "dns" {
   }
 
   memory {
-    dedicated = 512
-    floating  = 512
+    dedicated = 1024
+    floating  = 1024
   }
 
   network_device {
