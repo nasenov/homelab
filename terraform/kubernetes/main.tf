9e3cf30fc71fb39eddc59e18eec2129e68463d66
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2025-11-29 02:10:53 +0200

    chore: update renovate config

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -1,6 +1,6 @@
 data "talos_image_factory_extensions_versions" "this" {
   # renovate: datasource=docker depName=ghcr.io/siderolabs/installer
-  talos_version = "v1.11.5"
+  talos_version = "v1.11.4"
 
   filters = {
     names = [
