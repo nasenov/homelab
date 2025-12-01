51b3b8ef2ffc74bd04c0eaf401c5f9badabce2e0
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2025-12-01 19:41:53 +0200

    chore: terraform proxmox apt repositories

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,3 +1,32 @@
+resource "proxmox_virtual_environment_apt_standard_repository" "no_subscription" {
+  node   = "pve"
+  handle = "no-subscription"
+}
+
+resource "proxmox_virtual_environment_apt_standard_repository" "enterprise" {
+  node   = "pve"
+  handle = "enterprise"
+}
+
+resource "proxmox_virtual_environment_apt_repository" "enterprise" {
+  enabled   = false
+  file_path = proxmox_virtual_environment_apt_standard_repository.enterprise.file_path
+  index     = proxmox_virtual_environment_apt_standard_repository.enterprise.index
+  node      = proxmox_virtual_environment_apt_standard_repository.enterprise.node
+}
+
+resource "proxmox_virtual_environment_apt_standard_repository" "ceph_quincy_enterprise" {
+  node   = "pve"
+  handle = "ceph-squid-enterprise"
+}
+
+resource "proxmox_virtual_environment_apt_repository" "ceph_quincy_enterprise" {
+  enabled   = false
+  file_path = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.file_path
+  index     = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.index
+  node      = proxmox_virtual_environment_apt_standard_repository.ceph_quincy_enterprise.node
+}
+
 resource "proxmox_virtual_environment_acme_account" "letsencrypt" {
   name      = "letsencrypt"
   contact   = "asenov.nikolay98@gmail.com"
