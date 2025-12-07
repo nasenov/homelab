6d171424f1f31586e231eec992db2aafb74c8a70
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2025-12-07 22:57:42 +0200

    chore: use 16 GB memory per node

--- terraform/kubernetes/locals.tf
+++ terraform/kubernetes/locals.tf
@@ -3,7 +3,7 @@ locals {
     k8s-1 = {
       ipv4_address = "192.168.0.21"
       cpu_cores    = 4
-      memory       = 32768
+      memory       = 16384
       hostpci      = ["ceph1"]
       gpu          = ["gpu"]
       usb          = []
@@ -11,7 +11,7 @@ locals {
     k8s-2 = {
       ipv4_address = "192.168.0.22"
       cpu_cores    = 4
-      memory       = 32768
+      memory       = 16384
       hostpci      = ["ceph2"]
       gpu          = []
       usb          = ["zigbee"]
@@ -19,7 +19,7 @@ locals {
     k8s-3 = {
       ipv4_address = "192.168.0.23"
       cpu_cores    = 4
-      memory       = 32768
+      memory       = 16384
       hostpci      = ["ceph3"]
       gpu          = []
       usb          = []
