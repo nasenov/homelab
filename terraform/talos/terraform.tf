fb42186124f0991040aa1a5eac0c6d726b3114ec
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-04-15 20:04:28 +0300

    chore: rename kubernetes terraform

--- terraform/talos/terraform.tf
+++ terraform/talos/terraform.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = "~> 1.0"
+
   backend "s3" {
     region                      = "auto"
     bucket                      = "tfstate"
-    key                         = "kubernetes/terraform.tfstate"
+    key                         = "talos/terraform.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
